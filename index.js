10852a9a9ec8a5a1938d35ec1a27cf270195fb3a
SIta Dabo <94798285+obaddabo@users.noreply.github.com>
2023-12-05 12:48:48 +0100

    almost done

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const path = require('path');
+const connectdb = require('./config/connectdb');
 const port = process.env.PORT || 5000
+const mongoose = require('mongoose');
+const Task = require('./model/Task');
+const bodyParser = require('body-parser');
 
 
 
@@ -15,28 +19,58 @@ const port = process.env.PORT || 5000
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
 
-    res.render('index');
+    try {
+        const tasks = await Task.find();
+        res.render('index', { tasks });
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
 
 });
+
+connectdb();
 
 
 
     
 
-app.get('/createtask', (req, res) => {
+app.get('/task/add', (req, res) => {
 
     res.render('create_task');
 
 });
 
 
-app.post('/createtask', (req, res) => {
-    
+app.post('/task/add', async (req, res) => {
+    console.log(req.body)
+    const task = new Task({ name: req.body.name });
+   
+  
+    try {
+      const newTask = await task.save();
+      res.redirect('/');
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
 })
+
+// toggle completion
+
+app.post('/toggle/:id', async (req, res) => {
+    try {
+      const task = await Task.findById(req.params.id);
+      task.completed = !task.completed;
+      await task.save();
+      res.redirect('/');
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
 app.listen(port, (req, res) => {
     console.log(`Server is runing.... , listening on port ${port}`);
---
+++ model/Task.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+    const taskSchema = new mongoose.Schema({
+
+                name: {
+                        type : 'String',
+                        required : true
+                },
+
+                completed : {type : 'boolean', default : false},
+
+    })
+
+            module.exports = mongoose.model('Task', taskSchema);
